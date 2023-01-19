c7ca3bc0cb003a31a7d3ac746769c74f54351c42
Benoit Jaunin <jaunin.b@fr.ibm.com>
2023-01-19 09:18:11 +0100

    fix: add a boolean to link to existing DOI Toolchain to prevent count unresolved resource dependency

--- variables.tf
+++ variables.tf
@@ -202,8 +202,14 @@ variable "authorization_policy_creation" {
   default = ""
 }
 
+variable "link_to_doi_toolchain" {
+  description = "Enable a link to a DevOpsInsights instance in another toolchain, true or false"
+  type        = bool
+  default     = false
+}
+
 variable "doi_toolchain_id" {
   type = string
-  description = "DevOpsInsights Toolchain ID"
+  description = "DevOpsInsights Toolchain ID to link to"
   default = ""  
 }
