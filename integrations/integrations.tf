630d8a5ae4471e329730106fc52d84d676c73b70
huayuenh <huayuenh@ie.ibm.com>
2023-09-13 15:02:55 +0100

    chore: move outputs to outputs.tf

--- integrations/integrations.tf
+++ integrations/integrations.tf
@@ -152,29 +152,3 @@ resource "ibm_cd_toolchain_tool_eventnotifications" "cd_toolchain_tool_eventnoti
   }
   toolchain_id = var.toolchain_id
 }
-
-output "ibm_cd_toolchain_tool_artifactory" {
-  value = (var.enable_artifactory) ? ibm_cd_toolchain_tool_artifactory.cd_toolchain_tool_artifactory_instance[0].tool_id : null
-}
-
-
-output "secret_tool" {
-  value = (var.enable_key_protect) ? local.kp_integration_name : local.sm_integration_name
-  # Before returning this tool integration name
-  # used to construct {vault:: secret references,
-  # the authorization_policy must have been successfully created,
-  # and the tool integration must have been created,
-  # otherwise the secret references would not resolve and
-  # other tools using secret references could give errors during tool integration creation
-  depends_on = [
-    ibm_iam_authorization_policy.toolchain_secretsmanager_auth_policy,
-    ibm_iam_authorization_policy.toolchain_keyprotect_auth_policy,
-    ibm_cd_toolchain_tool_secretsmanager.secretsmanager,
-    ibm_cd_toolchain_tool_keyprotect.keyprotect
-  ]
-  description = "Used as part of secret references to point to the secret store tool integration"
-}
-
-output "sonarqube_tool" {
-  value = (var.sonarqube_config == "custom") ? ibm_cd_toolchain_tool_sonarqube.cd_toolchain_tool_sonarqube_instance[0].tool_id : null
-}
--- services/services.tf
+++ services/services.tf
@@ -50,8 +50,3 @@ data "ibm_resource_instance" "key_protect_instance" {
   resource_group_id = data.ibm_resource_group.kp_resource_group[0].id
   location          = var.kp_location
 }
-
-output "kp_instance_guid" {
-  value       = try(data.ibm_resource_instance.key_protect_instance[0].guid, "")
-  description = "GUID of the Key Protect service instance in IBM Cloud"
-}
