ff85a56fc2ed831aa3493bcf3d0f1db952555600
HUA YUEN HUI <HUAYUENH@ie.ibm.com>
2024-09-16 11:42:38 +0100

    chore: fix json property overrides (#78)

--- customizations/properties/property.tf
+++ customizations/properties/property.tf
@@ -57,6 +57,11 @@ locals {
 
   #add_property = ((local.is_valid_type == true) && (local.is_name_valid == true) && (local.is_enum == true))
   resolved_locked = ((local.input_locked == "true") || (local.input_locked == "false")) ? local.input_locked : "false"
+
+  #special handling
+  #allow set of Terraform variables to set values
+
+  special_value = try(var.property_data.config_data[local.input_name], "")
 }
 
 resource "ibm_cd_tekton_pipeline_property" "pipeline_property" {
@@ -64,7 +69,7 @@ resource "ibm_cd_tekton_pipeline_property" "pipeline_property" {
   pipeline_id = var.pipeline_id
   name        = local.input_name
   type        = local.input_type
-  value       = local.resolved_value
+  value       = (local.resolved_value == "") ? local.special_value : local.resolved_value
   path        = local.resolved_path
   enum        = local.input_enum
   locked      = local.resolved_locked
