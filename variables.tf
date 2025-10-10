1245b536ee0f1b9cef665af8a206c69fb0f1489f
huayuenh <huayuenh@ie.ibm.com>
2025-10-10 11:06:51 +0100

    chore: update readme, defaults

--- variables.tf
+++ variables.tf
@@ -1522,5 +1522,5 @@ variable "repo_integration_owner" {
 variable "use_legacy_ref" {
   type        = bool
   description = "Set to `true` to use the legacy secret reference format for Secrets Manager secrets."
-  default     = true
+  default     = false
 }
